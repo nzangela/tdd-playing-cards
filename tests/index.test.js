46d66454813929219f546f9dedefb2b34051e238
Angela Yang <kyan211@mywhitecliffe.com>
2021-10-20 21:42:00 +1300

    test for test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,12 @@
+const deck = require('../deck')
+
 test('test setup working', () => {
     expect(true).toBeTruthy()
+  })
+
+  test('returns a card', () => {
+    const amount = 1
+    const expected = "Ace of Spades"
+    const actual = deck.getCards(amount)
+    expect(actual).toBe(expected)
   })
